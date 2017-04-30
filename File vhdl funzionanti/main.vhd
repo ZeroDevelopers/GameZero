6f234c0b6abe2fd9f5430e148735c89c9470f498
hugrave <edoardo.conte@live.it>
2017-04-30 19:20:42 +0200

    Update

--- File vhdl funzionanti/main.vhd
+++ File vhdl funzionanti/main.vhd
@@ -25,14 +25,6 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity main is
     Port (  clk : in STD_LOGIC;
@@ -48,8 +40,10 @@ architecture Behavioral of main is
 
 component graphic is
     Port (  pixel_clk : in STD_LOGIC;
-            player1_pos : in std_logic_vector (18 downto 0);
-            player1_reversed : in std_logic;
+            GreenGoblin_pos : in std_logic_vector (18 downto 0);
+            GreenGoblin_reversed : in std_logic;
+            SilverSurfer_pos : in std_logic_vector (18 downto 0);
+            SilverSurfer_reversed : in std_logic;
             red : out STD_LOGIC_VECTOR (3 downto 0);
             green : out STD_LOGIC_VECTOR (3 downto 0);
             blue : out STD_LOGIC_vector (3 downto 0);
@@ -64,15 +58,22 @@ Port (  clk_in1 : in std_logic;
       );
 end component;
 
+--Constants
+constant WALL_WIDTH : natural := 20;
 
 -- signals to create the FRAME_CLOCK
 constant FRAME_PIXELS : natural := 420000;
 signal frame_clk_cnt : natural range 0 to FRAME_PIXELS / 2;
+
 signal pixel_clk, frame_clk : std_logic;
 
--- Signals for the player 1
-signal player1_pos : std_logic_vector (18 downto 0);
-signal player1_reversed : std_logic;
+-- Signals for the Green Goblin
+signal GreenGoblin_pos : std_logic_vector (18 downto 0) := "1100100000000000000";
+signal GreenGoblin_reversed : std_logic := '1';  -- At the normal orientation it is towrd left
+
+-- Signals for the Silver Surfer
+signal SilverSurfer_pos : std_logic_vector (18 downto 0) := "1001011000000000000";
+signal SilverSurfer_reversed : std_logic := '0';  -- At the normal orientation it is towrd right
 
 begin
 
@@ -89,25 +90,52 @@ begin
     end if;
 end process;
 
--- Process to move the ploayer 1
-process(frame_clk, player1_reversed)
+
+-- Process to move the Green Goblin
+
+process(frame_clk, GreenGoblin_reversed)
 begin
     if rising_edge(frame_clk) then
-        if player1_reversed = '0' and player1_pos (9 downto 0) = (640 - 63) then
-                player1_reversed <= '1';
-        elsif player1_reversed = '1' and player1_pos = 63 then
-            player1_reversed <= '0';
+        if GreenGoblin_reversed = '0' and GreenGoblin_pos (9 downto 0) = (WALL_WIDTH) then
+                GreenGoblin_reversed <= '1';
+        elsif GreenGoblin_reversed = '1' and GreenGoblin_pos (9 downto 0) = (640 - 63 - WALL_WIDTH) then
+            GreenGoblin_reversed <= '0';
         end if;
     end if;
 end process;
 
-process(frame_clk, player1_reversed)
+process(frame_clk, GreenGoblin_reversed)
 begin
     if rising_edge(frame_clk) then
-        if player1_reversed = '0' then
-            player1_pos <= player1_pos +1;
+        if GreenGoblin_reversed = '1' then
+            GreenGoblin_pos <= GreenGoblin_pos +1;
         else
-            player1_pos <= player1_pos -1;
+            GreenGoblin_pos <= GreenGoblin_pos -1;
+        end if;
+    end if;
+end process;
+
+
+-- Process to move the Silver Surfer
+
+process(frame_clk, SilverSurfer_reversed)
+begin
+    if rising_edge(frame_clk) then
+        if SilverSurfer_reversed = '1' and SilverSurfer_pos (9 downto 0) = (WALL_WIDTH) then
+                SilverSurfer_reversed <= '0';
+        elsif SilverSurfer_reversed = '0' and SilverSurfer_pos (9 downto 0) = (640 - 63 - WALL_WIDTH) then
+            SilverSurfer_reversed <= '1';
+        end if;
+    end if;
+end process;
+
+process(frame_clk, SilverSurfer_reversed)
+begin
+    if rising_edge(frame_clk) then
+        if SilverSurfer_reversed = '0' then
+            SilverSurfer_pos <= SilverSurfer_pos +1;
+        else
+            SilverSurfer_pos <= SilverSurfer_pos -1;
         end if;
     end if;
 end process;
@@ -124,14 +152,16 @@ port map
 
 inst_graphic : graphic
 port map
-(   pixel_clk => pixel_clk,
-    player1_pos => player1_pos,
-    player1_reversed => player1_reversed,
-    red => red,
-    green => green,
-    blue => blue,
-    HS => HS,
-    VS => VS
+(   pixel_clk               => pixel_clk,
+    GreenGoblin_pos         => GreenGoblin_pos,
+    GreenGoblin_reversed    => GreenGoblin_reversed,
+    SilverSurfer_pos        => SilverSurfer_pos,
+    SilverSurfer_reversed   => SilverSurfer_reversed,
+    red                     => red,
+    green                   => green,
+    blue                    => blue,
+    HS                      => HS,
+    VS                      => VS
 );
 
 
