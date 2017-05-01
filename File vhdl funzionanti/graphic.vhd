f8f8d3c37f329906bbfc2902a02e86b6d12fccb3
hugrave <edoardo.conte@live.it>
2017-05-01 23:25:58 +0200

    Update

--- File vhdl funzionanti/graphic.vhd
+++ File vhdl funzionanti/graphic.vhd
@@ -39,7 +39,10 @@ entity graphic is
             GreenGoblin_pos : in std_logic_vector (18 downto 0);
             GreenGoblin_reversed : in std_logic;
             SilverSurfer_pos : in std_logic_vector (18 downto 0);
-            SilverSurfer_reversed : in std_logic; 
+            SilverSurfer_reversed : in std_logic;
+            SilverSurfer_image : in std_logic_vector (1 downto 0); -- The corresponding image from which we have to extract the pixels
+            FireBall_pos : in std_logic_vector (18 downto 0);
+            FireBall_active : in std_logic;
             red : out STD_LOGIC_VECTOR (3 downto 0);
             green : out STD_LOGIC_VECTOR (3 downto 0);
             blue : out STD_LOGIC_vector (3 downto 0);
@@ -76,12 +79,34 @@ component SilverSurfer_rom is
            pixel_out : out STD_LOGIC_VECTOR (11 downto 0));
 end component;
 
+component SilverSurfer_2_rom is
+    Port ( address : in STD_LOGIC_VECTOR (11 downto 0);
+           pixel_out : out STD_LOGIC_VECTOR (11 downto 0));
+end component;
+
+component SilverSurfer_3_rom is
+    Port ( address : in STD_LOGIC_VECTOR (11 downto 0);
+           pixel_out : out STD_LOGIC_VECTOR (11 downto 0));
+end component;
+
+component SilverSurfer_4_rom is
+    Port ( address : in STD_LOGIC_VECTOR (11 downto 0);
+           pixel_out : out STD_LOGIC_VECTOR (11 downto 0));
+end component;
+
+component SilverSurfer_fire_rom is
+    Port ( address : in STD_LOGIC_VECTOR (9 downto 0);
+           pixel_out : out STD_LOGIC_VECTOR (11 downto 0));
+end component;
+
+
 
 -- Constants
 constant PLAYER_SIZE : natural := 64;
 constant SCREEN_WIDTH : natural := 640;
 constant SCREEN_HEIGHT : natural := 480;
 constant WALL : natural := 20;  -- Width of the wall at the borders of the screen
+constant FIRE_SIZE : natural := 20;
 
 --Signals for the vga
 signal pixel_in : std_logic_vector (11 downto 0) := (others => '0');
@@ -106,13 +131,17 @@ signal GreenGoblin_col : std_logic_vector (9 downto 0) := (others => '0');
 
 --Signals for Silver Surfer
 signal SilverSurfer_address : std_logic_vector (11 downto 0);
-signal SilverSurfer_pixel : std_logic_vector (11 downto 0);
+signal SilverSurfer_pixel, SilverSurfer_1_pixel, SilverSurfer_2_pixel, SilverSurfer_3_pixel, SilverSurfer_4_pixel : std_logic_vector (11 downto 0);
 signal SilverSurfer_row : std_logic_vector (8 downto 0) := (others => '0');
 signal SilverSurfer_col : std_logic_vector (9 downto 0) := (others => '0');
 
+-- Signals for the Silver Surfer Fire Ball
+signal FireBall_address : std_logic_vector (9 downto 0);
+signal FireBall_pixel : std_logic_vector (11 downto 0);
+
 
 -- Control signals for objects
--- Player 1
+-- Green Goblin
 signal GreenGoblin_enable : std_logic := '0';
 signal GreenGoblin_colored : std_logic;
 signal GreenGoblin_cntH : std_logic_vector (5 downto 0) := (others => '0');
@@ -124,6 +153,11 @@ signal SilverSurfer_colored : std_logic;
 signal SilverSurfer_cntH : std_logic_vector (5 downto 0) := (others => '0');
 signal SilverSurfer_cntV : std_logic_vector (5 downto 0) := (others => '0');
 
+-- Fire Ball
+signal FireBall_enable : std_logic := '0';
+signal FireBall_colored : std_logic;
+signal FireBall_cntH : std_logic_vector (4 downto 0) := (others => '0');
+signal FireBall_cntV : std_logic_vector (4 downto 0) := (others => '0');
 
 begin
 
@@ -167,7 +201,7 @@ end process;
 ---------------------------------------
 
 
--- Handling the counter for player 1
+-- Handling the counter for the Green Goblin
 process(pixel_clk, GreenGoblin_enable)
 begin
     if rising_edge(pixel_clk) and GreenGoblin_enable = '1' then
@@ -185,16 +219,16 @@ begin
     end if;
 end process;
 
--- defining the player 1 enabler
+-- defining the Green Goblin enabler
 GreenGoblin_enable <= '1' when (map_row - GreenGoblin_pos(18 downto 10)) < PLAYER_SIZE and (map_col - GreenGoblin_pos(9 downto 0)) < PLAYER_SIZE
                     else '0';
 
--- Defining the address in rom of the player 1
+-- Defining the address in rom of the Green Goblin
 GreenGoblin_address(11 downto 6) <= GreenGoblin_cntV;
 GreenGoblin_address(5 downto 0) <= GreenGoblin_cntH when GreenGoblin_reversed = '0'
                                 else PLAYER_SIZE - 1 - GreenGoblin_cntH;
 
--- Visibility of the player 1
+-- Visibility of the Green Goblin
 GreenGoblin_colored <= '0' when GreenGoblin_pixel = "111111111111"
                     else '1';
                     
@@ -232,9 +266,52 @@ SilverSurfer_address(11 downto 6) <= SilverSurfer_cntV;
 SilverSurfer_address(5 downto 0) <= SilverSurfer_cntH when SilverSurfer_reversed = '0'
                                 else PLAYER_SIZE - 1 - SilverSurfer_cntH;
 
+-- Choosing among the different images
+SilverSurfer_pixel <= SilverSurfer_1_pixel when SilverSurfer_image = "00"
+                    else SilverSurfer_2_pixel when SilverSurfer_image = "01"
+                    else SilverSurfer_3_pixel when SilverSurfer_image = "10"
+                    else SilverSurfer_4_pixel when SilverSurfer_image = "11";
+                    
 -- Visibility of Silver Surfer
 SilverSurfer_colored <= '0' when SilverSurfer_pixel = "111111111111"
                     else '1';
+                    
+
+---------------------------------------
+
+---   FIRE BALL processing
+
+---------------------------------------
+process(pixel_clk, FireBall_enable)
+begin
+    if rising_edge(pixel_clk) and FireBall_enable = '1' then
+        if (FireBall_cntH = FIRE_SIZE -1) then
+            if (FireBall_cntV < FIRE_SIZE -1) then
+                FireBall_cntH <= (others => '0');
+                FireBall_cntV <= FireBall_cntV +1;
+            else
+               FireBall_cntH <= (others => '0');
+               FireBall_cntV <= (others => '0');
+            end if;
+        else 
+            FireBall_cntH <= FireBall_cntH +1;
+        end if;
+    end if;
+end process;
+
+-- defining the Fire Ball enabler
+FireBall_enable <= '1' when FireBall_active = '1' and (map_row - FireBall_pos(18 downto 10)) < FIRE_SIZE and (map_col - FireBall_pos(9 downto 0)) < FIRE_SIZE
+                    else '0';
+
+-- Defining the address in rom of Silver Surfer
+FireBall_address(9 downto 5) <= FireBall_cntV;
+FireBall_address(4 downto 0) <= FireBall_cntH when SilverSurfer_reversed = '0'  -- When Silver Surfer is reversed I need also the image of the ball to be reversed
+                                else FIRE_SIZE - 1 - FireBall_cntH;
+
+                    
+-- Visibility of Silver Surfer
+FireBall_colored <= '0' when FireBall_pixel = "111111111111"
+                    else '1';            
 
   
 ------------------------------------------------------------------------
@@ -245,8 +322,9 @@ SilverSurfer_colored <= '0' when SilverSurfer_pixel = "111111111111"
 
 pixel_in <= GreenGoblin_pixel when GreenGoblin_enable = '1' and GreenGoblin_colored = '1'
             else SilverSurfer_pixel when SilverSurfer_enable = '1' and SilverSurfer_colored = '1'
+            else FireBall_pixel when FireBall_enable = '1' and FireBall_colored = '1'
             else "001100110011" when map_row < WALL or map_col < WALL or map_col > SCREEN_WIDTH - WALL -1 or map_row > SCREEN_HEIGHT - WALL -1
-            else "000011110000"; -- Projecting the background in the final project
+            else "000000001111"; -- Projecting the background in the final project
             
 --map_address (18 downto 10) <= map_row;
 --map_address (9 downto 0) <= map_col;
@@ -281,7 +359,31 @@ port map (
 inst_SilverSurfer : SilverSurfer_rom
 port map (
     address     => SilverSurfer_address,
-    pixel_out   => SilverSurfer_pixel
+    pixel_out   => SilverSurfer_1_pixel
+);
+
+inst_SilverSurfer_2 : SilverSurfer_2_rom
+port map (
+    address     => SilverSurfer_address,
+    pixel_out   => SilverSurfer_2_pixel
+);
+
+inst_SilverSurfer_3 : SilverSurfer_3_rom
+port map (
+    address     => SilverSurfer_address,
+    pixel_out   => SilverSurfer_3_pixel
+);
+
+inst_SilverSurfer_4 : SilverSurfer_4_rom
+port map (
+    address     => SilverSurfer_address,
+    pixel_out   => SilverSurfer_4_pixel
+);
+
+inst_SilverSurfer_fire : SilverSurfer_fire_rom
+port map (
+    address     => FireBall_address,
+    pixel_out   => FireBall_pixel
 );
 
 
--- File vhdl funzionanti/main.vhd
+++ File vhdl funzionanti/main.vhd
@@ -44,6 +44,9 @@ component graphic is
             GreenGoblin_reversed : in std_logic;
             SilverSurfer_pos : in std_logic_vector (18 downto 0);
             SilverSurfer_reversed : in std_logic;
+            SilverSurfer_image : in std_logic_vector (1 downto 0);
+            FireBall_pos : in std_logic_vector (18 downto 0);
+            FireBall_active : in std_logic;
             red : out STD_LOGIC_VECTOR (3 downto 0);
             green : out STD_LOGIC_VECTOR (3 downto 0);
             blue : out STD_LOGIC_vector (3 downto 0);
@@ -60,20 +63,37 @@ end component;
 
 --Constants
 constant WALL_WIDTH : natural := 20;
+constant PLAYER_SIZE : natural := 64;
+constant FIRE_SIZE : natural := 20;
+constant DELAY : natural := 1;
+constant SCREEN_WIDTH : natural := 640;
+constant SCREEN_HEIGHT : natural := 480;
+
 
 -- signals to create the FRAME_CLOCK
 constant FRAME_PIXELS : natural := 420000;
 signal frame_clk_cnt : natural range 0 to FRAME_PIXELS / 2;
 
 signal pixel_clk, frame_clk : std_logic;
+signal frame_mov_cnt : std_logic;
 
 -- Signals for the Green Goblin
 signal GreenGoblin_pos : std_logic_vector (18 downto 0) := "1100100000000000000";
 signal GreenGoblin_reversed : std_logic := '1';  -- At the normal orientation it is towrd left
 
 -- Signals for the Silver Surfer
-signal SilverSurfer_pos : std_logic_vector (18 downto 0) := "1001011000000000000";
+signal SilverSurfer_pos : std_logic_vector (18 downto 0) := "1001011000000010100";
 signal SilverSurfer_reversed : std_logic := '0';  -- At the normal orientation it is towrd right
+signal SilverSurfer_image : std_logic_vector (1 downto 0) := "00";
+constant SS_ACTION_FRAMES : natural := 8;
+signal SS_action_cnt : natural range 0 to SS_ACTION_FRAMES -1;
+signal SilverSurfer_fires : std_logic := '0';
+
+
+-- Sinals for the Fire Ball
+signal FireBall_active, FireBall_end, FireBall_start : std_logic := '0';
+signal FireBall_pos : std_logic_vector (18 downto 0) := "0000000000000000000";
+signal FireBall_cnt : natural := 0;
 
 begin
 
@@ -96,9 +116,9 @@ end process;
 process(frame_clk, GreenGoblin_reversed)
 begin
     if rising_edge(frame_clk) then
-        if GreenGoblin_reversed = '0' and GreenGoblin_pos (9 downto 0) = (WALL_WIDTH) then
+        if GreenGoblin_reversed = '0' and GreenGoblin_pos (9 downto 0) <= (WALL_WIDTH) then
                 GreenGoblin_reversed <= '1';
-        elsif GreenGoblin_reversed = '1' and GreenGoblin_pos (9 downto 0) = (640 - 63 - WALL_WIDTH) then
+        elsif GreenGoblin_reversed = '1' and GreenGoblin_pos (9 downto 0) >= (640 - 63 - WALL_WIDTH) then
             GreenGoblin_reversed <= '0';
         end if;
     end if;
@@ -107,35 +127,126 @@ end process;
 process(frame_clk, GreenGoblin_reversed)
 begin
     if rising_edge(frame_clk) then
-        if GreenGoblin_reversed = '1' then
-            GreenGoblin_pos <= GreenGoblin_pos +1;
+        if frame_mov_cnt = '1' then
+            if GreenGoblin_reversed = '1' then
+                GreenGoblin_pos <= GreenGoblin_pos +2;
+            else
+                GreenGoblin_pos <= GreenGoblin_pos -2;
+            end if;
         else
-            GreenGoblin_pos <= GreenGoblin_pos -1;
+            frame_mov_cnt <= not frame_mov_cnt;
         end if;
     end if;
 end process;
 
 
--- Process to move the Silver Surfer
+---- Process to move the Silver Surfer
 
-process(frame_clk, SilverSurfer_reversed)
+--process(frame_clk, SilverSurfer_reversed)
+--begin
+--    if rising_edge(frame_clk) then
+--        if SilverSurfer_reversed = '1' and SilverSurfer_pos (9 downto 0) = (WALL_WIDTH) then
+--                SilverSurfer_reversed <= '0';
+--        elsif SilverSurfer_reversed = '0' and SilverSurfer_pos (9 downto 0) = (640 - 63 - WALL_WIDTH) then
+--            SilverSurfer_reversed <= '1';
+--        end if;
+--    end if;
+--end process;
+
+--process(frame_clk, SilverSurfer_reversed)
+--begin
+--    if rising_edge(frame_clk) then
+--        if SilverSurfer_reversed = '0' then
+--            SilverSurfer_pos <= SilverSurfer_pos +1;
+--        else
+--            SilverSurfer_pos <= SilverSurfer_pos -1;
+--        end if;
+--    end if;
+--end process;
+
+
+-- Process to make the Silver Surfer fire
+process (frame_clk, SilverSurfer_image, SilverSurfer_reversed, FireBall_active)
 begin
     if rising_edge(frame_clk) then
-        if SilverSurfer_reversed = '1' and SilverSurfer_pos (9 downto 0) = (WALL_WIDTH) then
-                SilverSurfer_reversed <= '0';
-        elsif SilverSurfer_reversed = '0' and SilverSurfer_pos (9 downto 0) = (640 - 63 - WALL_WIDTH) then
-            SilverSurfer_reversed <= '1';
+        if SS_action_cnt = SS_ACTION_FRAMES -1 then
+            if SilverSurfer_image = "00" and FireBall_start = '1' then
+                FireBall_start <= '0';
+            elsif SilverSurfer_image = "11" and FireBall_start = '0' then
+                FireBall_start <= '1';
+                SilverSurfer_image <= "00";
+            else
+                if FireBall_active = '0' and SilverSurfer_fires = '1' then
+                    SilverSurfer_image <= SilverSurfer_image +1;
+                end if;
+            end if;
+            SS_action_cnt <= 0;
+        else
+            SS_action_cnt <= SS_action_cnt +1;
         end if;
     end if;
 end process;
 
-process(frame_clk, SilverSurfer_reversed)
+--process (frame_clk, FireBall_active, SilverSurfer_pos, delay_cnt)
+--begin
+--    if rising_edge (frame_clk) then
+--        if FireBall_active = '1' and delay_cnt = DELAY then
+--            if FireBall_pos(9 downto 0) > WALL_WIDTH then
+--                FireBall_active <= '0';
+--                SilverSurfer_canFire <= '1';
+--                delay_cnt <= 0;
+--            else
+--                FireBall_pos <= FireBall_pos +1;
+--            end if;
+--        elsif FireBall_active = '1' and delay_cnt < DELAY then
+--            delay_cnt <= delay_cnt +1;
+--        end if;  
+--    end if;
+--end process;
+
+
+FireBall_active <= '0' when FireBall_pos = "0000000000000000000"
+                    else '1';
+
+
+process(frame_clk, FireBall_active)
 begin
     if rising_edge(frame_clk) then
-        if SilverSurfer_reversed = '0' then
-            SilverSurfer_pos <= SilverSurfer_pos +1;
+        if FireBall_active = '1' then
+            if FireBall_pos(9 downto 0) >= SCREEN_WIDTH - WALL_WIDTH - FIRE_SIZE then
+                FireBall_end <= '1';
+            else
+                FireBall_end <= '0';
+            end if;
+        end if;
+    end if;
+end process;
+
+process (frame_clk, FireBall_active, FireBall_end, FireBall_start)
+begin
+    if rising_edge(frame_clk) then
+        if frame_mov_cnt = '1' then
+            if FireBall_start = '1' and FireBall_active = '0' then
+                FireBall_pos <= SilverSurfer_pos + PLAYER_SIZE - FIRE_SIZE;
+            elsif FireBall_end = '1' then
+                FireBall_pos <= "0000000000000000000";
+            elsif FireBall_active = '1' then
+                FireBall_pos <= FireBall_pos +2;
+            end if;
+        end if;
+    end if;
+end process;
+
+
+
+process(frame_clk)
+begin
+    if rising_edge(frame_clk) then
+        if FireBall_cnt = 120 then
+            FireBall_cnt <= 0;
+            SilverSurfer_fires <= not SilverSurfer_fires;
         else
-            SilverSurfer_pos <= SilverSurfer_pos -1;
+            FireBall_cnt <= FireBall_cnt +1;
         end if;
     end if;
 end process;
@@ -157,6 +268,9 @@ port map
     GreenGoblin_reversed    => GreenGoblin_reversed,
     SilverSurfer_pos        => SilverSurfer_pos,
     SilverSurfer_reversed   => SilverSurfer_reversed,
+    SilverSurfer_image      => SilverSurfer_image,
+    FireBall_active         => FireBall_active,
+    FireBall_pos            => FireBall_pos,
     red                     => red,
     green                   => green,
     blue                    => blue,
